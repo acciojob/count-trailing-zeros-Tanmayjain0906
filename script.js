2ea804e9248798c9f6ce125fabca435246ab1681
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-12-13 15:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+    const factorial = findFactorial(n);
+	const trailingZeros = findTrailingZeros(factorial);
+	const tbody = document.getElementsByTagName("tbody")[0];
+	const tr = document.createElement("tr");
+	const td1 = document.createElement("td");
+	td1.innerText = `trailingZeros(${n})`
+
+	const td2 = document.createElement("td");
+	td2.innerText = trailingZeros;
+
+	tr.append(td1, td2);
+	tbody.append(tr);
+	return trailingZeros;
+}
+
+function findFactorial(n) {
+	let ans = 1;
+	for(let i=1;i<=n;i++)
+		{
+			ans = ans*i;
+		}
+	return ans;
+}
+
+function findTrailingZeros(n) {
+    let count = 0;
+	while(true)
+		{
+			let rem = n%10;
+			if(rem == 0)
+			{ 
+				count++;
+				n = parseInt(n/10);
+			}
+			else
+			{
+				break;
+			}
+		}
+	return count;
 }
 
 const input = prompt("Enter a number");
